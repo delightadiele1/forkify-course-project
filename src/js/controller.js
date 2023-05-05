c68facbf464a386d5c8069d97e161dcd61af5dd2
delightadiele1 <delightadiele54@gmail.com>
2023-05-04 21:48:04 -1200

    added new welcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,6 +130,9 @@ const controlAddRecipe = async function(newRecipe) {
   }
 };
 
+const newFeature = function() {
+  console.log('Welcome to the application');
+};
 // showSearchRecipe();
 const init = function() {
   bookmarksView.addHandlerRender(controlBookmarkOnLoad);
@@ -139,7 +142,7 @@ const init = function() {
   searchView.addHandlerSearch(showSearchRecipe);
   paginationView.addHandlerPages(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome! to git');
+  newFeature();
 };
 init();
 
